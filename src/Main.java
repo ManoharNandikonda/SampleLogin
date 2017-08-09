391b590686527776ef67f9fd74bc80b5eac46ed6
harsha <ande.sriharsha@gmail.com>
2017-08-09 12:57:20 -0500

    Added new method methodRankPoints

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,18 @@ public class Main {
         System.out.println("Hello Everyone!");
         System.out.println("Hello E Gaints");
         System.out.println("Hello E Gaints1");
+        methodRankPoints(255.7);
+    }
+    /*
+        Created by Harsha
+     */
+    public static void methodRankPoints(double points) {
+        if (points >= 202.5) {
+            System.out.println("Rank:A1");
+        }else if (points >= 122.4) {
+            System.out.println("Rank:A2");
+        }else {
+            System.out.println("Rank:A3");
+        }
     }
 }
