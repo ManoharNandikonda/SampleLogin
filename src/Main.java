a18768732c088628d0cfcc84f87eb6ef1d90eb2a
harsha <ande.sriharsha@gmail.com>
2017-08-09 14:11:34 -0500

    Added new class MinNumber

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-        /*
-            created by Harsha
-         */
-        System.out.println("Hello Everyone!");
-        System.out.println("Hello E Gaints");
-        System.out.println("Hello E Gaints1");
+
         methodRankPoints(255.7);
+        MinNumber mini = new MinNumber();
+        mini.minFunction(2,3);
     }
     /*
         Created by Harsha
---
+++ src/MinNumber.java
@@ -0,0 +1,18 @@
+/**
+ * Created by harsha on 8/9/17.
+ */
+public class MinNumber {
+
+    public static void  minFunction(int n1, int n2) {
+        int min;
+        if (n1 > n2)
+            min = n2;
+        else
+            min = n1;
+
+        System.out.println(min);
+
+    }
+
+
+}
